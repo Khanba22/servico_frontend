f248e3004051d88e6ded4d4c66276909a1af3396
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2024-04-30 11:37:08 +0530

    made hero section responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -39,13 +39,13 @@ const Hamburger = () => {
 function Navbar() {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.navList}>
+      <div className={styles.navList}>
             <a href="">Home</a>
             <a href="">Services</a>
             <a href="">Contact Us</a>
             <a href="">About</a>
             <img src="" alt="" />
-        </div> */}
+        </div>
       <Hamburger />
     </div>
   );
